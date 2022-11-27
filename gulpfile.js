652efe4af2f8be165703ca62b86bc5e44fb79351
Brad Bell <brad@pixelandtonic.com>
2022-11-27 15:49:24 -0800

    ci update

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 
 behavePath = 'bower_components/behave.js';
 
-gulp.task('behave.js', function() {
-  return gulp.src([behavePath+'/*.js', behavePath+'/*.map'])
+gulp.task('behave.js', function () {
+  return gulp
+    .src([behavePath + '/*.js', behavePath + '/*.map'])
     .pipe(gulp.dest('lib/behave.js'));
 });
 
